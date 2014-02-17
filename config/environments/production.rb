934ae87c2d284fd18ab10f6df389b7568a7d7fab
edEssential <ed@essential-music.com>
2014-02-17 09:33:56 +0000

    s3 bucket creation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ V1::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
   config.action_mailer.smtp_settings = {
     :address => "smtp.mandrillapp.com",
     :port => 587,
