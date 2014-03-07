c8729641a5fe48df65ab3d76def188060712a49a
edEssential <ed@essential-music.com>
2014-03-07 12:11:56 +0000

    header social links changed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,4 +28,15 @@ $(document).ready(function(){
 		});   
 		
 	 });
+	
+	//// Launch all external links in _blank windows!!
+	$("a").click(function() {
+		link_host = this.href.split("/")[2];
+	    document_host = document.location.href.split("/")[2];
+
+	    if (link_host != document_host) {
+	      window.open(this.href);
+	      return false;
+	    }
+	});
 });
